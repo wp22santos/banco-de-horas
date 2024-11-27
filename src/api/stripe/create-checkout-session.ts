cce50293e7037dec0de5b11f44e2a5ff7a9accef
Welington <wp22santos@gmail.com>
2024-11-27 19:16:30 -0300

    fix: atualiza versão da API do Stripe

--- src/api/stripe/create-checkout-session.ts
+++ src/api/stripe/create-checkout-session.ts
@@ -1,7 +1,7 @@
 import Stripe from 'stripe';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2023-10-16',
+  apiVersion: '2024-11-20.acacia',
 });
 
 export default async function handler(req: any, res: any) {
